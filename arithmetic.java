9a0c1fa0c64157bf1c88abe49a5c1a815e0f0250
jackiewirt <42756157+jackiewirt@users.noreply.github.com>
2018-08-29 13:45:18 -0700

    Add files via upload

---
+++ arithmetic.java
@@ -0,0 +1,20 @@
+class Arithmetic {
+public static void main (String[] args) {
+	int a = 1;
+	int b = 20;
+	float x = 4.5f;
+	float y = 6.9f;
+	
+	System.out.println("a is " + a + ", b is " + b);
+	System.out.println("x is " + x + ", y is " + y);
+	
+	System.out.println("a + b = " + (a + b));
+	System.out.println("x + y = " + (x + y));
+	System.out.println("a / b = " + (a / b));
+	System.out.println("b / a = " + (b / a));
+	System.out.println("x / y = " + (x / y));
+	System.out.println("y / a = " + (y / x));
+
+
+}
+}
