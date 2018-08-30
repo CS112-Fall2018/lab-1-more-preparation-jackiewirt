cf4f98cc781ec73c1ed4bad33e91033864c092d9
jackiewirt <42756157+jackiewirt@users.noreply.github.com>
2018-08-29 19:15:49 -0700

    Add files via upload

--- arithmetic.java
+++ arithmetic.java
@@ -9,11 +9,26 @@ public static void main (String[] args) {
 	System.out.println("x is " + x + ", y is " + y);
 	
 	System.out.println("a + b = " + (a + b));
+	System.out.println("a + x = " + (a + x));
+	System.out.println("a + y = " + (a + y));	
+	System.out.println("b + x = " + (b + x));
+	System.out.println("b + y = " + (b + y));
 	System.out.println("x + y = " + (x + y));
+
 	System.out.println("a / b = " + (a / b));
 	System.out.println("b / a = " + (b / a));
 	System.out.println("x / y = " + (x / y));
-	System.out.println("y / a = " + (y / x));
+	System.out.println("y / x = " + (y / x));
+	System.out.println("a / y = " + (a / y));
+	System.out.println("y / a = " + (y / a));
+	System.out.println("a / x = " + (a / x));
+	System.out.println("x / a = " + (x / a));
+	System.out.println("b / y = " + (b / y));
+	System.out.println("y / b = " + (y / b));
+	System.out.println("b / x = " + (b / x));
+	System.out.println("x / b = " + (x / b));
+
+
 
 
 }
